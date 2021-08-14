86c98510ff8e06ffa49aac47b77afb751e248a9b
11SF <nawaphong.sit@dome.tu.ac.th>
2021-08-15 02:26:03 +0700

    release

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:nextflow_covid_today/covid_today_result.dart';
+import 'package:nextflow_covid_today/stat_box.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,11 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Nextflow COVID-19 Today',
+      title: 'Covid-19 Status',
       theme: ThemeData(
         primarySwatch: Colors.blue
       ),
-      home: MyHomePage(title: 'Nextflow COVID-19 Today'),
+      home: MyHomePage(title: 'Covid-19 Status'),
     );
   }
 }
@@ -27,17 +30,57 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   
+  @override
+  void initState() { 
+    super.initState();
+    getData();
+  }
+
+  Future<CovidTodayResult> getData() async {
+    var response = await http.get('https://covid19.th-stat.com/api/open/today');
+    final result = covidTodayResultFromJson(response.body);
+    return result;
+  }
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: ListView(
-        children: <Widget>[
-        ],
-      )
+      body: FutureBuilder(
+        future: getData(),
+        builder: (BuildContext context, AsyncSnapshot<CovidTodayResult> snapshot) { 
+          if(snapshot.connectionState == ConnectionState.done) {
+
+            final result = snapshot.data;
+            
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: <Widget>[
+                  StatBox(title: 'ผู้ติดเชื้อสะสม', total: result?.confirmed, backgroundColor: Color(0xffFF173E)),
+                    SizedBox(height: 10,),
+                    
+                  StatBox(title: 'หายแล้ว', total: result?.recovered, backgroundColor: Color(0xff46E36D)),
+                    SizedBox(height: 10,),
+
+                  StatBox(title: 'กำลังรักษา', total: result?.hospitalized, backgroundColor: Color(0xff43A1D9)),
+                    SizedBox(height: 10,),
+
+                  StatBox(title: 'จำนวนผู้เสียชีวิต', total: result?.deaths, backgroundColor: Color(0xffE68A1C)),
+                  ListTile(
+                    title: Text('ข้อมูลจากกรมควบคุมโรค ณ วันที่ '),
+                    subtitle: Text('${result?.updateDate ?? 'กำลังโหลด'}'),
+                  )          
+                ],
+              ),
+            );
+          }
+
+        return LinearProgressIndicator();
+      },),
     );
   }
 }
---
+++ lib/stat_box.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class StatBox extends StatelessWidget {
+
+  final String title;
+  final int total;
+  final Color backgroundColor;
+
+  const StatBox({Key key, this.title, this.total, this.backgroundColor}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          color: backgroundColor,
+          borderRadius: BorderRadius.circular(10)
+        ),
+
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.end,                      
+          children: <Widget>[
+            Text(title,
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white
+              ),
+             ),
+            Expanded(
+               child: Text(
+                 '${NumberFormat('#,###').format(total) ?? 'กำลังโหลด'}',
+                 style: TextStyle(
+                   fontSize: 48,
+                   color: Colors.white
+                ),
+                textAlign: TextAlign.right,
+               ),
+             )
+           ],
+        )            
+    );
+  }
+}
